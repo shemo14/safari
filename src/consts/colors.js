4b8b066e1ee4c666afc0e7053b9d5ce2c432070f
Mohamed Shams <mo7amed.shams3477@gmail.com>
2020-09-17 16:23:19 +0200

    first commit

---
+++ src/consts/colors.js
@@ -0,0 +1,12 @@
+export default {
+    light_gray          : '#a7a7a7',
+    black_gray          : '#6a6a6a',
+    red                 : '#d3292a',
+    light_red           : '#f4c9c9',
+    green               : '#007F00',
+
+    orange      :'#ffb247',
+    blue        :'#19BBC1',
+    black       :'#2C2C2C',
+    gray        :'#959595',
+}
